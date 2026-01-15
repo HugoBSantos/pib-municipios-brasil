e6267d5709831f20dd8f7e9195ba71783c595d2d
HugoSantos14 <hugosantos14000@gmail.com>
2026-01-15 20:43:37 -0300

    feat: create dimensional table for time

--- sql/ddl/create_silver.sql
+++ sql/ddl/create_silver.sql
@@ -35,3 +35,17 @@ CREATE TABLE IF NOT EXISTS silver.dim_uf AS (
             ('TO', 'Tocantins')
     ) AS t(sigla_uf, nome_uf)
 );
+
+CREATE TABLE IF NOT EXISTS silver.dim_tempo AS (
+    SELECT
+        ROW_NUMBER() OVER() AS ano_id,
+        *
+    FROM (
+        VALUES
+            (2002), (2003), (2004), (2005), (2006),
+            (2007), (2008), (2009), (2010), (2011),
+            (2012), (2013), (2014), (2015), (2016),
+            (2017), (2018), (2019), (2020), (2021),
+            (2022), (2023)
+    ) AS t(ano)
+);
