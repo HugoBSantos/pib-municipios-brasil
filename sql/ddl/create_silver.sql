d85b961c5df576e7f808f71c3e48c1a75ff295d1
HugoSantos14 <hugosantos14000@gmail.com>
2026-02-13 23:12:01 -0300

    feat: create physical data model to silver layer

--- sql/ddl/create_silver.sql
+++ sql/ddl/create_silver.sql
@@ -1,51 +1,58 @@
 CREATE SCHEMA IF NOT EXISTS silver;
 
-CREATE TABLE IF NOT EXISTS silver.dim_uf AS (
-    SELECT
-        ROW_NUMBER() OVER() AS uf_id,
-        *
-    FROM (
-        VALUES
-            ('AC', 'Acre'),
-            ('AL', 'Alagoas'),
-            ('AM', 'Amazonas'),
-            ('AP', 'Amapá'),
-            ('BA', 'Bahia'),
-            ('CE', 'Ceará'),
-            ('DF', 'Distrito Federal'),
-            ('ES', 'Espírito Santo'),
-            ('GO', 'Goiás'),
-            ('MA', 'Maranhão'),
-            ('MG', 'Minas Gerais'),
-            ('MS', 'Mato Grosso do Sul'),
-            ('MT', 'Mato Grosso'),
-            ('PA', 'Pará'),
-            ('PB', 'Paraíba'),
-            ('PE', 'Pernambuco'),
-            ('PI', 'Piauí'),
-            ('PR', 'Paraná'),
-            ('RJ', 'Rio de Janeiro'),
-            ('RN', 'Rio Grande do Norte'),
-            ('RO', 'Rondônia'),
-            ('RR', 'Roraima'),
-            ('RS', 'Rio Grande do Sul'),
-            ('SC', 'Santa Catarina'),
-            ('SE', 'Sergipe'),
-            ('SP', 'São Paulo'),
-            ('TO', 'Tocantins')
-    ) AS t(sigla_uf, nome_uf)
+CREATE TABLE IF NOT EXISTS silver.ufs (
+    uf_id SERIAL NOT NULL,
+    nome_uf VARCHAR(20) NOT NULL,
+    sigla_uf CHAR(2) NOT NULL,
+
+    CONSTRAINT pk_uf_id PRIMARY KEY (uf_id),
+    CONSTRAINT un_uf_nome UNIQUE (nome_uf),
+    CONSTRAINT un_uf_sigla UNIQUE (sigla_uf)
 );
 
-CREATE TABLE IF NOT EXISTS silver.dim_ano AS (
-    SELECT
-        ROW_NUMBER() OVER() AS ano_id,
-        *
-    FROM (
-        VALUES
-            (2002), (2003), (2004), (2005), (2006),
-            (2007), (2008), (2009), (2010), (2011),
-            (2012), (2013), (2014), (2015), (2016),
-            (2017), (2018), (2019), (2020), (2021),
-            (2022), (2023)
-    ) AS t(ano)
+CREATE TABLE IF NOT EXISTS silver.municipios (
+    municipio_id SERIAL NOT NULL,
+    nome_municipio VARCHAR(50) NOT NULL,
+    uf_id INTEGER NOT NULL,
+
+    CONSTRAINT pk_mun_id PRIMARY KEY (municipio_id),
+    CONSTRAINT fk_mun_uf FOREIGN KEY (uf_id) REFERENCES silver.ufs (uf_id)
+);
+
+CREATE TABLE IF NOT EXISTS silver.anos (
+    ano_id SERIAL NOT NULL,
+    ano INTEGER NOT NULL,
+
+    CONSTRAINT pk_ano_id PRIMARY KEY (ano_id),
+    CONSTRAINT un_ano UNIQUE (ano)
+);
+
+CREATE TABLE IF NOT EXISTS silver.pib (
+    municipio_id INTEGER NOT NULL,
+    ano_id INTEGER NOT NULL,
+    valor_pib NUMERIC(15,3),
+
+    CONSTRAINT pk_pib_id PRIMARY KEY (municipio_id, ano_id),
+    CONSTRAINT fk_pib_mun FOREIGN KEY (municipio_id) REFERENCES silver.municipios (municipio_id),
+    CONSTRAINT fk_pib_ano FOREIGN KEY (ano_id) REFERENCES silver.anos (ano_id)
+);
+
+CREATE TABLE IF NOT EXISTS silver.impostos (
+    municipio_id INTEGER NOT NULL,
+    ano_id INTEGER NOT NULL,
+    valor_imp NUMERIC(15,3),
+
+    CONSTRAINT pk_imp_id PRIMARY KEY (municipio_id, ano_id),
+    CONSTRAINT fk_imp_mun FOREIGN KEY (municipio_id) REFERENCES silver.municipios (municipio_id),
+    CONSTRAINT fk_imp_ano FOREIGN KEY (ano_id) REFERENCES silver.anos (ano_id)
+);
+
+CREATE TABLE IF NOT EXISTS silver.valor_adicionado (
+    municipio_id INTEGER NOT NULL,
+    ano_id INTEGER NOT NULL,
+    valor_add NUMERIC(15,3),
+
+    CONSTRAINT pk_add_id PRIMARY KEY (municipio_id, ano_id),
+    CONSTRAINT fk_add_mun FOREIGN KEY (municipio_id) REFERENCES silver.municipios (municipio_id),
+    CONSTRAINT fk_add_ano FOREIGN KEY (ano_id) REFERENCES silver.anos (ano_id)
 );
