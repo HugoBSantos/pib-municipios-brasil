c1e79ff20b36879ee2cc38c28c26c7246c6aa5a7
HugoSantos14 <hugosantos14000@gmail.com>
2026-02-13 23:15:38 -0300

    feat: create dml script for inserting initial data in the 'anos' and 'ufs' tables on silver layer

---
+++ sql/dml/silver_dml.sql
@@ -0,0 +1,39 @@
+INSERT INTO
+    silver.anos (ano)
+VALUES
+    (2002), (2003), (2004), (2005), (2006),
+    (2007), (2008), (2009), (2010), (2011),
+    (2012), (2013), (2014), (2015), (2016),
+    (2017), (2018), (2019), (2020), (2021),
+    (2022), (2023);
+
+INSERT INTO
+    silver.ufs (nome_uf, sigla_uf)
+VALUES
+    ('Acre', 'AC'),
+    ('Alagoas', 'AL'),
+    ('Amazonas', 'AM'),
+    ('Amapá', 'AP'),
+    ('Bahia', 'BA'),
+    ('Ceará', 'CE'),
+    ('Distrito Federal', 'DF'),
+    ('Espírito Santo', 'ES'),
+    ('Goiás', 'GO'),
+    ('Maranhão', 'MA'),
+    ('Minas Gerais', 'MG'),
+    ('Mato Grosso do Sul', 'MS'),
+    ('Mato Grosso', 'MT'),
+    ('Pará', 'PA'),
+    ('Paraíba', 'PB'),
+    ('Pernambuco', 'PE'),
+    ('Piauí', 'PI'),
+    ('Paraná', 'PR'),
+    ('Rio de Janeiro', 'RJ'),
+    ('Rio Grande do Norte', 'RN'),
+    ('Rondônia', 'RO'),
+    ('Roraima', 'RR'),
+    ('Rio Grande do Sul', 'RS'),
+    ('Santa Catarina', 'SC'),
+    ('Sergipe', 'SE'),
+    ('São Paulo', 'SP'),
+    ('Tocantins', 'TO');
