60edc2d27e7c3e8fd838f69a288dbbe819b17bc1
HugoSantos14 <hugosantos14000@gmail.com>
2026-02-17 16:07:39 -0300

    refactor: remove try/except block to propagate exception

--- src/data_pipeline/data_loading.py
+++ src/data_pipeline/data_loading.py
@@ -9,26 +9,22 @@ POSTGRES_URL = os.getenv("POSTGRES_URL")
 def load_to_postgres(connection: DuckDBPyConnection, schema: str, tables: list[str]):
     POSTGRES_DDL_PATH = f"sql/ddl/create_{schema}_postgres.sql"
     
-    try:
-        with psycopg.connect(POSTGRES_URL) as pg_conn:
-            with pg_conn.cursor() as cur:
-                with open(POSTGRES_DDL_PATH, mode="r") as f:
-                    cur.execute(f.read())
-            pg_conn.commit()
+    with psycopg.connect(POSTGRES_URL) as pg_conn:
+        with pg_conn.cursor() as cur:
+            with open(POSTGRES_DDL_PATH, mode="r") as f:
+                cur.execute(f.read())
+        pg_conn.commit()
+    
+    connection.execute(f"""
+        ATTACH '{POSTGRES_URL}' AS postgres
+        (TYPE postgres)
+    """)
+    
+    for table in tables:
+        duckdb_table = f"{schema}.{table}"
+        postgres_table = f"postgres.{duckdb_table}"
         
         connection.execute(f"""
-            ATTACH '{POSTGRES_URL}' AS postgres
-            (TYPE postgres)
+            INSERT INTO {postgres_table}
+            SELECT * FROM {duckdb_table}
         """)
-        
-        for table in tables:
-            duckdb_table = f"{schema}.{table}"
-            postgres_table = f"postgres.{duckdb_table}"
-            
-            connection.execute(f"""
-                INSERT INTO {postgres_table}
-                SELECT * FROM {duckdb_table}
-            """)
-        
-    except Exception as e:
-        raise TypeError("Failed to load data to PostgresSQL") from e
